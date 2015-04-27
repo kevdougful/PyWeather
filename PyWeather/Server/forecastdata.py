b9d5cb45a0a6ebf0fb2b6174fbae00115b83ce38
Kevin Coffey <kevin.d.coffey@monsanto.com>
2015-04-27 15:09:21 -0500

    system in place for building ForecastDay objects

--- PyWeather/Server/forecastdata.py
+++ PyWeather/Server/forecastdata.py
@@ -1,34 +1,49 @@
 from getxml import xml_request
 from datetime import datetime
 
-# get element(s) from XML
-def _getelements(xml, element):
-    'returns a specific element from some XML'
-    return xml.getElementsByTagName(element)
-
 class Forecast(object):
     '''
     encapsulates a series of ForecastDay 
     objects
     '''
     def __init__(self, location):
-        self.xml = xml_request('forecast', location)
-        self.txt_forecast = _getelements(self.xml, 'txt_forecast')
-        self.simpleforecast = _getelements(self.xml, 'simpleforecast')
-        #days = self.txt_forecast[0].childNodes[1].getElementsByTagName('forecastday')
-        #print(len(days))
-        days = self.xml.getElementsByTagName('forecastday')
-        print(days[0].toprettyxml())
-        #print(len(days))
-        #for item in self.txt_forecast[0].childNodes:
-        #    if item.nodeType == 1:
-        #        days = item.getElementsByTagName('forecastday')
-        #        print(len(days))
+        xml = xml_request('forecast', location)
+        txt_forecast = xml.getElementsByTagName('txt_forecast')
+        simpleforecast = xml.getElementsByTagName('simpleforecast')
+        
+        # get all forecastday elements from simple forecast
+        simplexml = list()
+        for simpleday in simpleforecast[0].getElementsByTagName('forecastday'):
+            simplexml.append(simpleday)
+
+        txtdays = txt_forecast[0].getElementsByTagName('forecastday')
+
+        # get day forecastday elements from text forecast
+        dayxml = list()
+        for i in range(0, 8, 2):
+            dayxml.append(txtdays[i])
+
+        # get night forecastday elements from text forecast
+        nightxml = list()
+        for i in range(1, 9, 2):
+            nightxml.append(txtdays[i])
+
+        #print(len(simplexml))
+        #print(len(dayxml))
+        #print(len(nightxml))
+
+        # create ForecastDay objects
+        for i in range(0, 4):
+            ForecastDay(dayxml[i], nightxml[i], simplexml[i])
 
 class ForecastDay(object):
     '''
     encapsulates all forecast data from a
     Wunderground API response
     '''
-    def __init__(self, xml):
-        self.xml = xml
+    def __init__(self, day, night, simple):
+        epoch = simple.getElementsByTagName('epoch')[0].firstChild.nodeValue
+        self.date = datetime.fromtimestamp(int(epoch))
+        self.day_icon = day.getElementsByTagName('icon')[0].firstChild.nodeValue
+        self.nt_icon = night.getElementsByTagName('icon')[0].firstChild.nodeValue
+        print(self.nt_icon)
--- PyWeather/Server/server.py
+++ PyWeather/Server/server.py
@@ -1,6 +1,4 @@
 import getxml
 from forecastdata import Forecast
 
-forecast_xml = getxml.xml_request('forecast', 'MO/Maryland_Heights')
-print(forecast_xml)
 somedate = Forecast('MO/Maryland_Heights')
