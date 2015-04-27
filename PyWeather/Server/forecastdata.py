eadd77c5e60ed8e4f0aeeaa6276a08de7360e5c2
Kevin Coffey <kevin.d.coffey@monsanto.com>
2015-04-27 16:44:29 -0500

    fixed self-closing tags problem (sort of)

--- PyWeather/Server/forecastdata.py
+++ PyWeather/Server/forecastdata.py
@@ -2,7 +2,12 @@ from getxml import xml_request
 from datetime import datetime
 
 def _getNodeValue(element, tagname):
-    return element.getElementsByTagName(tagname)[0].firstChild.nodeValue
+    elemstr = element.toxml()
+    # handle self closing tags
+    if elemstr.find('/>', 0, len(elemstr)) > -1: 
+        return 0.0
+    else: 
+        return element.getElementsByTagName(tagname)[0].firstChild.nodeValue
 
 class Forecast(object):
     '''
@@ -48,7 +53,8 @@ class ForecastDay(object):
     '''
     def __init__(self, day, night, simple):
         # set the date
-        epoch = _getNodeValue(simple, 'epoch')
+        epoch = simple.getElementsByTagName('epoch')[0].firstChild.nodeValue
+        #epoch = _getNodeValue(simple, 'epoch')
         self.date = datetime.fromtimestamp(int(epoch))
 
         # txt_forecast elements
@@ -60,7 +66,8 @@ class ForecastDay(object):
         self.nightpop = int(_getNodeValue(night, 'pop'))
 
         # simpleforecast elements
-        self.period = int(_getNodeValue(simple, 'period'))
+        self.period = simple.getElementsByTagName('period')[0].firstChild.nodeValue
+        #self.period = int(_getNodeValue(simple, 'period'))
         
         high = simple.getElementsByTagName('high')
         self.high_F = int(_getNodeValue(high[0], 'fahrenheit'))
@@ -68,7 +75,8 @@ class ForecastDay(object):
         low = simple.getElementsByTagName('low')
         self.low_F = int(_getNodeValue(low[0], 'fahrenheit'))
         
-        self.humidity = _getNodeValue(simple, 'avehumidity')
+        self.humidity = simple.getElementsByTagName('avehumidity')[0].firstChild.nodeValue
+        #self.humidity = _getNodeValue(simple, 'avehumidity')
 
         qpf_allday = simple.getElementsByTagName('qpf_allday') # quantity precip. fallen
         self.qpf_allday_in = float(_getNodeValue(qpf_allday[0], 'in'))
@@ -82,10 +90,35 @@ class ForecastDay(object):
         maxwind = simple.getElementsByTagName('maxwind')
         self.maxwind_mph = int(_getNodeValue(maxwind[0], 'mph'))
         self.maxwind_degrees = float(_getNodeValue(maxwind[0], 'degrees'))
-        print(self.maxwind_degrees)
+        self.maxwind_dir = _getNodeValue(maxwind[0], 'dir')
 
         # metric values not implemented
         # snow_allday, snow_day, snow_night elements not implemented
 
     def __str__(self):
         return self.date.strftime('%Y-%m-%d')
+
+    #@staticmethod
+    #def degreesToDirection(deg):
+    #    '''
+    #    convert 0-360° float to three letter heading (e.g. NNE)
+    #    '''
+    #    if deg < 0 or deg > 360:
+    #        raise Exception('degree value must be 0-360')
+    #    elif deg >= 0 and deg <= 11.25: return 'N'
+    #    elif deg > 348.75 and deg <= 360: return "N"
+    #    elif deg > 11.25 and deg <= 33.75: return "NNE"
+    #    elif deg > 33.75 and deg <= 56.25: return "NE"
+    #    elif deg > 56.25 and deg <= 78.75: return "ENE"
+    #    elif deg > 78.75 and deg <= 101.25: return "E"
+    #    elif deg > 101.25 and deg <= 123.75: return "ESE"
+    #    elif deg > 123.75 and deg <= 146.25: return "SE"
+    #    elif deg > 146.25 and deg <= 168.75: return "SSE"
+    #    elif deg > 168.75 and deg <= 191.25: return "S"
+    #    elif deg > 191.25 and deg <= 213.75: return "SSW"
+    #    elif deg > 213.75 and deg <= 236.25: return "SW"
+    #    elif deg > 236.25 and deg <= 258.75: return "WSW"
+    #    elif deg > 258.75 and deg <= 281.25: return "W"
+    #    elif deg > 281.25 and deg <= 303.75: return "WNW"
+    #    elif deg > 303.75 and deg <= 326.25: return "NW"
+    #    elif deg > 326.25 and deg <= 348.75: return "NNW"
--- PyWeather/Server/server.py
+++ PyWeather/Server/server.py
@@ -4,4 +4,4 @@ from forecastdata import Forecast
 cast = Forecast('MO/Maryland_Heights')
 for day in cast.ForecastDays:
     print(str(day))
-    print(day.date.strftime('%A'))
+    print(day.maxwind_dir)
