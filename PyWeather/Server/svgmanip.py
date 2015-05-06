cebe81afa09d409c37988490417c98531a8e0c02
Kevin Coffey <kevin.d.coffey@monsanto.com>
2015-05-06 14:10:10 -0500

    Writes all forecast elements (initial)

--- PyWeather/Server/svgmanip.py
+++ PyWeather/Server/svgmanip.py
@@ -6,7 +6,7 @@ import codecs
 
 def write_forecast(forecast_obj):
     '''
-
+    Writes a Forecast object using template.svg
     '''
     # Open the SVG template (read-only)
     output = codecs.open('./template.svg', 'r', encoding='utf-8').read()
@@ -16,7 +16,7 @@ def write_forecast(forecast_obj):
 
 def write_forecastday(svg, forecastday_obj):
     '''
-    
+    Writes a ForecastDay object to the given svg object
     '''
     period = forecastday_obj.period
 
@@ -28,9 +28,10 @@ def write_forecastday(svg, forecastday_obj):
 
     # Text
     svg = svg.replace(period + 'title', forecastday_obj.date.strftime('%a %b-%d'))
-    # TODO: strip out Low/high wind info from text 
-    # TODO: parse day text into multiple lines
-    svg = svg.replace(period + 'nighttext', forecastday_obj.nighttext)
+    svg = svg.replace(period + 'daytext1', _parse_text(forecastday_obj.daytext)[0])
+    svg = svg.replace(period + 'daytext2', _parse_text(forecastday_obj.daytext)[1])
+    svg = svg.replace(period + 'nighttext1', _parse_text(forecastday_obj.nighttext)[0])
+    svg = svg.replace(period + 'nighttext2', _parse_text(forecastday_obj.nighttext)[1])
 
     # High, low, humidity
     svg = svg.replace(period + 'high', str(forecastday_obj.high_F) + 'F')
@@ -42,6 +43,30 @@ def write_forecastday(svg, forecastday_obj):
     svg = svg.replace(period + 'nightrainchance', str(forecastday_obj.nightpop) + '%')
     svg = svg.replace(period + 'dayrainamount', str(forecastday_obj.qpf_day_in) + '\"')
     svg = svg.replace(period + 'nightrainamount', str(forecastday_obj.qpf_night_in) + '\"')
-
-
     return svg
+
+def _parse_text(text):
+    '''
+    Parses forecast text: removes Low/High and Wind info
+    '''
+    new_end = text.find('High')
+    if new_end == -1:
+        new_end = text.find('Low')
+    if new_end > -1:
+        text = text[:new_end - 1]
+    return _wrap_text(text)
+
+def _wrap_text(text):
+    '''
+    Splits a long line of text into two lines
+    '''
+    breaklength = 30
+    wrapped_text = ['', '']
+    if len(text) <= breaklength:
+        # Don't wrap short strings
+        wrapped_text[0] = text
+    else:
+        br = text.find(' ', breaklength)
+        wrapped_text[0] = text[:br]
+        wrapped_text[1] = text[br:]
+    return wrapped_text
